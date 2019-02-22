ab9b91a71742471b3e5bc3e1496c6535392cf225
tcwatkins24 <tannerwatkins24@gmail.com>
2019-02-22 13:32:56 -0600

    added neccessary tables, may need to add attributes as neccessary

--- tables/driver.sql
+++ tables/driver.sql
@@ -1,1 +1,27 @@
+create table if not exist Driver (
+    Id int,
+    Dnamne varchar(25),
+    primary key  (Id)
+);
 
+create table if not exist Rider (
+    username varchar(25),
+    balance int,
+    Upassword varchar(25),
+    primary key (username)
+);
+
+create table if not exist Van (
+    VanId int,
+    numRiders int,
+    route int,
+    primary key (VanId),
+    foreign key (route)
+);
+
+create table if not exist Route (
+    route int,
+    startLoc location,
+    endLoc location,
+    primary key(route)
+);
