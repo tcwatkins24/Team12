02650587900208dd084bfa3c95789268cc86740c
K2mc484 <kylehall484@gmail.com>
2019-02-25 16:52:49 -0600

    H2 database
    The database is running and populates a table with basic information

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,27 +1,36 @@
-create table if not exist Driver (
-    Id int,
-    Dnamne varchar(25),
-    primary key  (Id)
+
+CREATE TABLE IF NOT EXISTS Driver (
+     id        INTEGER  PRIMARY KEY AUTO_INCREMENT,
+     dName VARCHAR(25),
 );
 
-create table if not exist Rider (
-    username varchar(25),
-    balance int,
-    Upassword varchar(25),
-    primary key (username)
-);
+INSERT INTO Driver (dName) VALUES('Kyle');
 
-create table if not exist Van (
-    VanId int,
-    numRiders int,
-    route int,
-    primary key (VanId),
-    foreign key (route)
-);
+--create table if not exist Driver (
+--    Id int,
+--    Dnamne varchar(25),
+--    primary key  (Id)
+--);
+--
+--create table if not exist Rider (
+--    username varchar(25),
+--    balance int,
+--    Upassword varchar(25),
+--    primary key (username)
+--);
+--
+--create table if not exist Van (
+--    VanId int,
+--    numRiders int,
+--    route int,
+--    primary key (VanId),
+--    foreign key (route)
+--);
+--
+--create table if not exist Route (
+--    route int,
+--    startLoc location,
+--    endLoc location,
+--    primary key(route)
+--);
 
-create table if not exist Route (
-    route int,
-    startLoc location,
-    endLoc location,
-    primary key(route)
-);
