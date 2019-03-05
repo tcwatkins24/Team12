4f25b0f5d198dd29357549c3072b2b10d4a0d29a
Xiaotao Li <xiaotao-li@outlook.com>
2019-03-05 01:03:17 -0600

    Add features on Sign Up page Modify signUpController Create UserDto, EmailSender

--- src/main/java/com/group12/vanPool/controllers/signUpController.java
+++ src/main/java/com/group12/vanPool/controllers/signUpController.java
@@ -1,16 +1,40 @@
 package com.group12.vanPool.controllers;
 
+import com.group12.vanPool.UserDto;
+import com.group12.vanPool.service.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.mail.MessagingException;
+import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/signup")
+@RequestMapping("")
 public class signUpController {
 
-    @GetMapping
-    public String signUp(Model model){
-        return "signup";
+    private EmailService emailService;
+
+    public signUpController( EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping(value = "/signUp")
+    public String showSignUpForm(WebRequest request, Model model) throws MessagingException {
+        UserDto userDto = new UserDto();
+        model.addAttribute("userDto", userDto);
+        return "signUp";
+    }
+
+    @PostMapping(value = "/submit-signUp")
+    public ModelAndView saveUser(ModelAndView modelAndView, @ModelAttribute("userDto") @Valid final UserDto userDto) {
+
+        modelAndView.setViewName("/signUp");
+        return modelAndView;
     }
 }
