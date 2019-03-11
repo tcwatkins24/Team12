d82f866e32696bda3e609a1f234e759b3a3a4cbe
Xiaotao Li <xiaotao-li@outlook.com>
2019-03-11 01:36:00 -0500

    add complete sign up page

--- src/main/java/com/group12/vanPool/controllers/signUpController.java
+++ src/main/java/com/group12/vanPool/controllers/signUpController.java
@@ -1,9 +1,9 @@
 package com.group12.vanPool.controllers;
 
 import com.group12.vanPool.UserDto;
-import com.group12.vanPool.service.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,23 +18,24 @@ import javax.validation.Valid;
 @RequestMapping("")
 public class signUpController {
 
-    private EmailService emailService;
 
-    public signUpController( EmailService emailService) {
-        this.emailService = emailService;
+    public signUpController( ) {
     }
 
     @GetMapping(value = "/signUp")
-    public String showSignUpForm(WebRequest request, Model model) throws MessagingException {
+    public String showSignUpForm(WebRequest request, Model model) {
         UserDto userDto = new UserDto();
         model.addAttribute("userDto", userDto);
         return "signUp";
     }
 
     @PostMapping(value = "/submit-signUp")
-    public ModelAndView saveUser(ModelAndView modelAndView, @ModelAttribute("userDto") @Valid final UserDto userDto) {
-
-        modelAndView.setViewName("/signUp");
+    public ModelAndView saveUser(ModelAndView modelAndView, @ModelAttribute("userDto") @Valid final UserDto userDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("/signUp");
+        }else {
+            modelAndView.setViewName("/completeSignUp");
+        }
         return modelAndView;
     }
 }
