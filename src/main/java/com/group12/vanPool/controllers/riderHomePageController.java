2e08af136ca60141001c1735b9132639a7435a86
K2mc484 <kylehall484@gmail.com>
2019-03-25 23:28:36 -0500

    Revert "removed mvc config  and fixed login"
    This reverts commit 4af0d27f061d1b45cf37900468e6fb0f4ad03d69.

--- src/main/java/com/group12/vanPool/controllers/riderHomePageController.java
+++ src/main/java/com/group12/vanPool/controllers/riderHomePageController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/riderHomePage")
 public class riderHomePageController {
 
+
     @GetMapping
     public String homePage(Model model){
         return "riderHomePage";
